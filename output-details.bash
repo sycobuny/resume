a7512cfa4b04fe9492e85a2eb9361584971a2927
Stephen Belcher <sycobuny@users.noreply.github.com>
2013-10-11 10:25:43 -0700

    Expand resume and add formatter
    Originally written via Gist in October 2013: The sections are now
    expanded, and there is a script to pull more-private data from the git
    environment (in case I am generating this myself, vs. someone else
    generating it). The sections are also expanded with more complete resume
    details.

---
+++ output-details.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# yes, I store my address and phone number in my git config; cause, why not?
+# it's as reasonable a private-ish key/value data store as any other.
+street=$(git config user.address)
+city=$(git config user.city)
+state=$(git config user.state)
+zip=$(git config user.zip)
+phone=$(git config user.phone)
+file=outputs/details.tex
+
+# I don't mind the general populace knowing I live in Baltimore, though; at
+# least, assuming I haven't moved and failed to update this script.
+city=${city:-Baltimore}
+state=${state:-MD}
+
+# if we have enough to build it, put the whole address in there; otherwise,
+# just the city and state (which we should have as a bare minimum)
+if [[ -n "$street" ]] && [[ -n "$zip" ]]; then
+    address="$street, $city, $state $zip"
+else
+    address="$city, $state"
+fi
+
+# address goes in the resume, whatever that wound up being
+echo "\\address{$address}" > $file
+
+# phone number only goes in if we got one
+if [[ "x$phone" != 'x' ]]; then
+    echo "\\phone{$phone}" >> $file
+fi
